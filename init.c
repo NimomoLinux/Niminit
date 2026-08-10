f87142c447548237674a400b8e602da52d00d527
Nimomo Linux <forrealemail8548@gmail.com>
2026-08-10 14:27:32 +0200

    Add files via upload

---
+++ init.c
@@ -0,0 +1,36 @@
+#include <sys/mount.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/reboot.h>
+#include <linux/reboot.h>
+
+int main(void) {
+    mount("proc", "/proc", "proc", 0, NULL);
+    mount("sysfs", "/sys", "sysfs", 0, NULL);
+    printf("NimomOS starting... Niminit succeeded...\n");
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("Fork failed");
+        return 1;
+    } else if (pid == 0) {
+        // --- CHILD PROCESS ---
+        // Replace this process with the shell
+        execl("/bin/sh", "sh", NULL);
+        perror("execl failed");
+        exit(1);
+    } else {
+        // --- PARENT PROCESS (PID 1) ---
+        // Wait for the child shell process (pid) to finish
+        waitpid(pid, NULL, 0);
+        
+        printf("Shell exited. Shutting down NimomOS...\n");
+        reboot(LINUX_REBOOT_CMD_POWER_OFF);
+        while(1) { sleep(1); }
+    }
+
+    return 0;
+}
